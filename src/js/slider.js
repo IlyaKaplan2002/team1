77f416deaa1cfa0b673db18b98138221f77ca989
IlyaKaplan2002 <kaplan11092002@gmail.com>
2021-09-08 01:19:22 +0300

    slider is done and some fixes

--- src/js/slider.js
+++ src/js/slider.js
@@ -27,13 +27,48 @@ $(document).ready(function () {
     // slidesToShow: 1,
     // slidesToScroll: 1,
     // centerMode: true,
+    // infinite:true,
     asNavFor: '.feedback__list',
+    autoplay: true,
+    autoplaySpeed: 1500,
+    // easing: cubic-bezier(0.4, 0, 0.2, 1),
 
     responsive: [
       {
         breakpoint: 766,
         settings: {
           arrows: true,
+        },
+      },
+    ],
+  });
+
+  $('.feedback__list').slick({
+    // nextArrow: next,
+    // prevArrow: prev,
+    // appendArrows: $('.feedback-slider'),
+    arrows: false,
+    // dots: false,
+    // mobileFirst: true,
+    // slidesToShow: 1,
+    // slidesToScroll: 1,
+    // centerMode: true,
+    // centerMode: true,
+    // focusOnSelect: true,
+    asNavFor: '.feedback__main-slider',
+    // autoplay: true,
+    // autoplaySpeed: 1500,
+    infinite:true,
+    slidesToShow: 6,
+    // easing: cubic-bezier(0.4, 0, 0.2, 1),
+    // centerMode: true,
+    focusOnSelect: true,
+
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 3,
         },
       },
     ],

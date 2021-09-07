5596b1d2af88be637667e5ee2993ad8695b28ce3
yaroslav.kosytsia <jaroslavkositsa@gmail.com>
2021-09-07 21:29:33 +0300

    add links to slider

--- src/js/slider.js
+++ src/js/slider.js
@@ -15,8 +15,26 @@
 // });
 
 $(document).ready(function () {
-    $('.slider').slick({
-        arrows: true,
-        dots: true,
-    });
+  const prev = $('.feedback__article-button--left');
+  const next = $('.feedback__article-button--right');
+
+  $('.feedback-slider').slick({
+    nextArrow: next,
+    prevArrow: prev,
+    appendArrows: $('.feedback-slider'),
+    arrows: false,
+    dots: false,
+    mobileFirst: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    centerMode: true,
+    responsive: [
+      {
+        breakpoint: 766,
+        settings: {
+          arrows: true,
+        },
+      },
+    ],
+  });
 });
